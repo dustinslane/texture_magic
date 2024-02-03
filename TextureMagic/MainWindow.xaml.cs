b9cc87b947d02e27eb78b58ebe46c3500e8106a6
Dustin Slane <info@dustinslane.nl>
2024-02-03 10:45:16 +0100

    version bump to 0.6

--- TextureMagic/MainWindow.xaml.cs
+++ TextureMagic/MainWindow.xaml.cs
@@ -55,7 +55,7 @@ namespace TextureMagic
         public MainWindow()
         {
             this.InitializeComponent();
-            this.Title = "Texture Magic by Dustin Slane ( v 0.5.0 )"; 
+            this.Title = "Texture Magic by Dustin Slane ( v 0.6.0 )"; 
             Progress.Value = 0;
             _cancellationTokenSource = new CancellationTokenSource();
             _worker.DoWork += WorkerOnDoWork;
